5268e3727b114c6ddb04776d4bc0216766fa821f
Nikita <thetax45@gmail.com>
2020-05-09 20:18:03 -0400

    updated main.py to be fully pylint approved

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+"""
+Primary driver.
+"""
 #selenium
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
+
 
 #pandas
 import pandas as pd
@@ -20,13 +19,13 @@ assert "Stock" in driver.title
 
 tbody = driver.find_element_by_tag_name("tbody")
 for row in tbody.find_elements_by_tag_name("tr"):
-   cells = row.find_elements_by_tag_name("td")
-   symbol = cells[0].text
-   name = cells[1].text
-   price = cells[2].text
-   change = cells[3].text
-   percent_change = cells[4].text
-   stock_list.append(Stock(symbol, name, price, change, percent_change))
+    cells = row.find_elements_by_tag_name("td")
+    symbol = cells[0].text
+    name = cells[1].text
+    price = cells[2].text
+    change = cells[3].text
+    percent_change = cells[4].text
+    stock_list.append(Stock(symbol, name, price, change, percent_change))
 
 top_names = []
 top_symbols = []
@@ -35,19 +34,19 @@ top_change = []
 top_percent_change = []
 
 for top_stock in stock_list[:5]:
-   top_names.append(top_stock.name)
-   top_symbols.append(top_stock.symbol)
-   top_price.append(top_stock.price)
-   top_change.append(top_stock.change)
-   top_percent_change.append(top_stock.percent_change)
+    top_names.append(top_stock.name)
+    top_symbols.append(top_stock.symbol)
+    top_price.append(top_stock.price)
+    top_change.append(top_stock.change)
+    top_percent_change.append(top_stock.percent_change)
 
 data_top_5 = {
-   'Name': top_names,
-   'Symbol':top_symbols,
-   'Price':top_price,
-   'Change':top_change,
-   'Change (%)':top_percent_change
-} 
+    'Name': top_names,
+    'Symbol':top_symbols,
+    'Price':top_price,
+    'Change':top_change,
+    'Change (%)':top_percent_change
+}
 
 df = pd.DataFrame(data_top_5, columns=['Name', 'Symbol', 'Price', 'Change', 'Change (%)'])
 print(df)
