a7df97c2f47eefe8ac3c0ed312c4afefe6f90a5b
git <thetax45@gmail.com>
2020-05-22 18:32:11 -0400

    cleaned up main branch

---
+++ app.py
@@ -0,0 +1,104 @@
+import os
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+import time, datetime
+
+
+
+app = Flask(__name__)
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+engine = create_engine(os.environ['DATABASE_URL'])
+
+Base = declarative_base()
+
+Base.metadata.reflect(engine)
+
+from models import Data, Infections
+
+
+
+
+dt = Data()
+
+def run_query(query):
+   return pd.read_sql(query, con=engine)
+
+@app.route("/")
+def hello():
+ 
+    print(dt.get_us_total_infections())
+    print(dt.get_monthly_totals(dt.get_us_total_infections()))
+
+
+    us_infections_monthly = dt.get_monthly_totals(dt.get_us_total_infections())
+    us_infections_monthly = dt.add_month_name_column(us_infections_monthly)
+    us_infections_monthly_html = dt.df_to_html(us_infections_monthly)
+    months = us_infections_monthly['month_name'].tolist()
+    cases = us_infections_monthly['cases'].tolist()
+
+
+    us_infections_daily = dt.get_daily_totals(dt.get_us_total_infections())
+    us_infections_daily = dt.add_day_name_column(us_infections_daily)
+    day = us_infections_daily['day_name'].tolist()
+    dcases = us_infections_daily['cases'].tolist()
+
+    return render_template('dashboard/dashboard.html', us_infections_monthly_html=us_infections_monthly_html, us_infections_monthly_list=[months,cases], us_infections_daily_list=[day, dcases]  )
+@app.route("/states.html")
+def get_states():
+    try:
+        states_infections_monthly = dt.get_monthly_totals_by_state()
+        all_province_states = states_infections_monthly[0]
+        all_province_states_stripped = [x.strip(' ') for x in all_province_states]
+
+        monthly_province_state_dfs = states_infections_monthly[1]
+        dats = []
+        #do this in models.py
+        for dat in monthly_province_state_dfs:
+            months = dat[['month_name']]
+            counts = dat[['cases']]
+            months = months.values.tolist() 
+            counts = counts.values.tolist() 
+            dat = [months, counts]
+            dats.append(dat)
+
+
+        states_infections_daily = dt.get_daily_totals_by_state()
+        all_province_states = states_infections_daily[0]
+        all_province_states_stripped = [x.strip(' ') for x in all_province_states]
+
+        daily_province_state_dfs = states_infections_daily[1]
+        datss = []
+        #do this in models.py
+        for dat in daily_province_state_dfs:
+            days = dat[['day_name']]
+            counts = dat[['cases']]
+            days = days.values.tolist() 
+            counts = counts.values.tolist() 
+            dat = [days, counts]
+            datss.append(dat)
+
+
+        
+
+        return render_template('dashboard/states.html', all_province_states=all_province_states, all_province_states_stripped=all_province_states_stripped, all_province_states_len=len(all_province_states),  monthly_province_state_dfs=dats,  daily_province_state_dfs=datss )
+    except Exception as e:
+	    return(str(e))
+
+
+@app.route("/about.html")
+def get_about():
+    try:
+        return render_template('dashboard/about.html')
+    except Exception as e:
+	    return(str(e))
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,111 @@
+from app import db
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+import time, datetime
+
+
+class Infections(db.Model):
+    __tablename__ = 'us_infections'
+
+    index = db.Column(db.Integer, primary_key=True)
+    combined_key = db.Column(db.String())
+    date = db.Column(db.String())
+    country_region = db.Column(db.String())
+    cases = db.Column(db.Integer())
+    province_state = db.Column(db.String())
+
+    def __init__(self, index):
+        self.index = index
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+        }
+
+
+class Data():
+    us_deaths = None
+    engine = None
+
+    def run_query(self, query):
+       return pd.read_sql(query, con=self.engine)
+
+    def __init__(self):
+        self.engine = create_engine(os.environ['DATABASE_URL'])
+
+    #Desc: Groups cases by day then selects rows for last day of each month
+    #Output: Dataframe
+    def get_monthly_totals(self, df):
+        df['day'] = pd.to_datetime(df['date']).dt.to_period('D')
+        df = df.iloc[df.reset_index().groupby(pd.to_datetime(df['date']).dt.to_period('M'))['index'].idxmax()]
+        return df
+
+    #Desc: Groups cases by day
+    #Output: Dataframe
+    def get_daily_totals(self, df):
+        df['day'] = pd.to_datetime(df['date']).dt.to_period('D')
+        return df
+
+    def get_states(self):
+        states_df = self.run_query('SELECT * FROM \"states\";')
+        return states_df.province_state.unique()
+   
+    #Desc: Filters df rows by state
+    #Updated Desc: Selects state table
+    #Output: Dataframe
+    def get_by_state(self, state):
+        states_df = self.run_query('SELECT * FROM \"'+state+'\";')
+        return states_df
+
+    def get_us_total_infections(self):
+        return self.run_query('SELECT * FROM \"us total\";')
+
+    def add_month_name_column(self, df):
+        df['day'] = pd.to_datetime(df['date']).dt.to_period('D')
+        df['month_name'] = df['day'].apply(lambda x: x.strftime('%b'))
+        return df
+
+    def add_day_name_column(self, df):
+        df['day'] = pd.to_datetime(df['date']).dt.to_period('D')
+        df['day_name'] = df['day'].apply(lambda x: x.strftime('%b %d'))
+        return df
+
+    #Desc: Converts df to bootstrap compatible html table
+    #Output: HTML table converted DataFrame
+    def df_to_html(self, df):
+        return df.to_html(index=False, classes=["table-bordered", "table-striped", "table-hover", "table-dark"])
+
+    #Desc: Maps get_monthly_totals to each state
+    #Output: List of [[States], [Dataframes]]
+    def get_monthly_totals_by_state(self):
+        all_province_states = self.get_states()
+        monthly_province_state_dfs = []
+        states = []
+        for province_state in all_province_states:
+            state_df = self.get_by_state(province_state)
+            state_monthly_total_df = self.get_monthly_totals(state_df)
+            state_monthly_total_df = self.add_month_name_column(state_monthly_total_df)
+            state_monthly_total_df = state_monthly_total_df[['cases', 'month_name']]
+            monthly_province_state_dfs.append(state_monthly_total_df)
+            states.append(province_state)
+        return [states, monthly_province_state_dfs]
+
+
+    #Desc: Maps get_monthly_totals to each state
+    #Output: List of [[States], [Dataframes]]
+    def get_daily_totals_by_state(self):
+        all_province_states = self.get_states()
+        monthly_province_state_dfs = []
+        states = []
+        for province_state in all_province_states:
+            state_df = self.get_by_state(province_state)
+            state_monthly_total_df = self.get_daily_totals(state_df)
+            state_monthly_total_df = self.add_day_name_column(state_monthly_total_df)
+            state_monthly_total_df = state_monthly_total_df[['cases', 'day_name']]
+            monthly_province_state_dfs.append(state_monthly_total_df)
+            states.append(province_state)
+        return [states, monthly_province_state_dfs]
