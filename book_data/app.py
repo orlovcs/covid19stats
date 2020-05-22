925a2fd079366bbb0ea676a77f09d4937c2127cd
git <thetax45@gmail.com>
2020-05-21 21:26:23 -0400

    last commit before gunicorn integration and deployment

--- book_data/app.py
+++ book_data/app.py
@@ -80,41 +80,13 @@ def get_states():
 	    return(str(e))
 
 
-@app.route("/get")
-def get_all():
+@app.route("/about.html")
+def get_about():
     try:
-        #inf=Infections.query.all()
-        #return  jsonify([e.serialize() for e in inf])
-        books_df = run_query('SELECT * FROM us_infections LIMIT 10;')
-        return render_template('thanks.html', books = books_df.to_html() )
-
-
-
+        return render_template('dashboard/about.html')
     except Exception as e:
 	    return(str(e))
 
-
-@app.route("/get/<id_>")
-def get_by_id(id_):
-    try:
-        return jsonify(Infections.serialize())
-    except Exception as e:
-	    return(str(e))
-
-@app.route("/add/form",methods=['GET', 'POST'])
-def add_book_form():
-    if request.method == 'POST':
-        name=request.form.get('name')
-        author=request.form.get('author')
-        published=request.form.get('published')
-        try:
-
-            db.session.add(book)
-            db.session.commit()
-            return "Book added. book id={}".format(book.id)
-        except Exception as e:
-            return(str(e))
-    return render_template("getdata.html")
 
 if __name__ == '__main__':
     app.run()
