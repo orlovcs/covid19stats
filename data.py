9740a690b3155e7bfe844ca20ad3143396c24987
git <thetax45@gmail.com>
2020-05-23 21:40:42 -0400

    updated data refresh method not have to load in 370k rows
    the csv will now be directly loaded into a df and subsequently used to refresh all tables

--- data.py
+++ data.py
@@ -9,7 +9,7 @@ import numpy as np
 import datetime
 import time
 from sqlalchemy.types import TEXT
-
+import requests
 
 #connect to the database
 connection = pg.connect("dbname=covid user=postgres password=root")
@@ -56,40 +56,54 @@ def csvloader(engine, file, table):
    except ValueError:
         print('Table '+table+' already exists, doing nothing')
       
-   #after main table is added, create the additional aggregated table
 
+#downloaded updated dataset
+response = requests.get('http://datahub.io/core/covid-19/r/us_confirmed.csv')
+file_object = io.StringIO(response.content.decode('utf-8'))
+us_confirmed_df = pd.read_csv(file_object)
 
+us_confirmed_df.columns = map(str.lower, us_confirmed_df.columns)
+#remove all whitespaces, replace them with underscores
+us_confirmed_df = us_confirmed_df.rename(columns=lambda x: x.replace(' ', '_'))
+us_confirmed_df = us_confirmed_df.rename(columns=lambda x: x.strip())
+us_confirmed_df = us_confirmed_df.rename(columns={'country/region': 'country_region', 'province/state': 'province_state', 'case':'cases'})
+us_confirmed_df = us_confirmed_df[['combined_key', 'date', 'country_region', 'cases', 'province_state']]
 
 
 #initiate loading csv
-csvloader(engine, '../datasets/us_confirmed.csv', 'us_infections')
+#csvloader(engine, '../datasets/us_confirmed.csv', 'us_infections')
 
-query = 'SELECT * FROM us_infections;'
-df = run_query(query)
-province_states = df.province_state.drop_duplicates()
+province_states = us_confirmed_df.province_state.drop_duplicates()
 
 #Create a table for all states
 table = 'states'
 try:
-   province_states.to_sql(table, engine, if_exists='fail')
+   province_states.to_sql(table, engine, if_exists='replace')
 except ValueError:
       print('Table '+table+' already exists, doing nothing')
 
 #Create total cases in America table
-query = 'SELECT date, SUM(cases) AS cases FROM us_infections GROUP BY date;'
-df = run_query(query)
+
+
+#Pandas equivalent statement for 'SELECT date, SUM(cases) AS cases FROM us_infections GROUP BY date;'
+# df = run_query(query)
+df = us_confirmed_df.groupby(['date'])['cases'].sum()
+
 table = 'us total'
 try:
-   df.to_sql(table, engine, if_exists='fail')
+   df.to_sql(table, engine, if_exists='replace')
 except ValueError:
       print('Table '+table+' already exists, doing nothing')
 
 #Create total cases table for every state
 for province_state in province_states:
-   query = 'SELECT date, SUM(cases) AS cases FROM us_infections WHERE province_state=\''+province_state+'\' GROUP BY date;'
-   df = run_query(query)
+   #Pandas equivalent statement for  'SELECT date, SUM(cases) AS cases FROM us_infections WHERE province_state=\''+province_state+'\' GROUP BY date;'
+   df = us_confirmed_df[us_confirmed_df.province_state == province_state]
+   df = df.groupby(['date'])['cases'].sum()
    table = province_state
    try:
-      df.to_sql(table, engine, if_exists='fail')
+      df.to_sql(table, engine, if_exists='replace')
    except ValueError:
          print('Table '+table+' already exists, doing nothing') 
+
+print("Tables Updated")
